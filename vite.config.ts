a0c415f6e914d4f83e60c771ada22499542ac36b
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-07-24 00:41:03 -0500

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+  plugins: [
+    react(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  root: ".",
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+  },
+  server: {
+    fs: {
+      strict: true,
+      deny: ["**/.*"],
+    },
+  },
+});
+
+
